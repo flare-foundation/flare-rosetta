2594b2c3574a90dd611d487de7e5ff6963b48830
Aljaz S <117018081+aljazs-flare@users.noreply.github.com>
2022-11-23 13:57:40 +0100

    take $MODE into account in healthcheck.sh

--- server/docker/healthcheck.sh
+++ server/docker/healthcheck.sh
@@ -3,38 +3,46 @@
 set -eo pipefail
 if [ "$DEBUG" = "true" ]; then set -o xtrace; fi
 
-export ROSETTA_FLARE_ENDPOINT=${ROSETTA_FLARE_ENDPOINT:?"Env var ROSETTA_FLARE_ENDPOINT is required!"}
 export NETWORK_ID=${NETWORK_ID:?"Env var NETWORK_ID is required!"}
 
+if [ "$MODE" = "online" ]; then
 
-STATUS=$(curl -m 10 -s -w %{http_code} ${ROSETTA_FLARE_ENDPOINT}/ext/health -o /dev/null)
-if [ $STATUS = "200" ]; then
-    echo "go-flare :: /ext/health :: ok"
-elif [ $STATUS = "503" ] && [ $NETWORK_ID = "localflare" ]; then
-    echo "go-flare :: /ext/health :: 503-localflare" 
-    is_because_of_no_peers=$(curl -s ${ROSETTA_FLARE_ENDPOINT}/ext/health | grep "network layer is unhealthy reason: not connected to a minimum of 1 peer")
-    if [ -z is_because_of_no_peers ]; then
+    export ROSETTA_FLARE_ENDPOINT=${ROSETTA_FLARE_ENDPOINT:?"Env var ROSETTA_FLARE_ENDPOINT is required!"}
+
+    STATUS=$(curl -m 10 -s -w %{http_code} ${ROSETTA_FLARE_ENDPOINT}/ext/health -o /dev/null)
+    if [ $STATUS = "200" ]; then
+        echo "go-flare :: /ext/health :: ok"
+    elif [ $STATUS = "503" ] && [ $NETWORK_ID = "localflare" ]; then
+        echo "go-flare :: /ext/health :: 503-localflare" 
+        is_because_of_no_peers=$(curl -s ${ROSETTA_FLARE_ENDPOINT}/ext/health | grep "network layer is unhealthy reason: not connected to a minimum of 1 peer")
+        if [ -z is_because_of_no_peers ]; then
+            exit 1
+        fi
+        echo "go-flare :: /ext/health :: ok"
+    else
+        echo "go-flare :: /ext/health :: FAIL"
         exit 1
     fi
-    echo "go-flare :: /ext/health :: ok"
+
+    DONE_BOOTSTRAPPING=$(curl -X POST --data '{"jsonrpc": "2.0","method": "info.isBootstrapped","params":{"chain":"C"},"id":1}' -H 'Content-Type: application/json' -s ${ROSETTA_FLARE_ENDPOINT}/ext/info | jq '.result.isBootstrapped')
+    if [ "$DONE_BOOTSTRAPPING" != "true" ]; then
+        echo "go-flare :: bootstrapping :: FAIL"
+        exit 1
+    fi
+    echo "go-flare :: bootstrapping :: ok"
 else
-    echo "go-flare :: /ext/health :: FAIL"
-    exit 1
+    echo "go-flare :: mode!=online - healthcheck disabled :: skipped"
 fi
 
-DONE_BOOTSTRAPPING=$(curl -X POST --data '{"jsonrpc": "2.0","method": "info.isBootstrapped","params":{"chain":"C"},"id":1}' -H 'Content-Type: application/json' -s ${ROSETTA_FLARE_ENDPOINT}/ext/info | jq '.result.isBootstrapped')
-if [ "$DONE_BOOTSTRAPPING" != "true" ]; then
-    echo "go-flare :: bootstrapping :: FAIL"
-    exit 1
+
+if [ "${VALIDATE_ROSETTA_HEALTH}" != "false" ]; then
+    if [ ! "$(curl -s http://localhost:8080/)" ]; then
+        echo "rosetta :: bound to port :: FAIL"
+        exit 1
+    fi
+    echo "rosetta :: bound to port :: ok"
+else
+    echo "rosetta :: healthcheck disabled :: skipped"
 fi
-    echo "go-flare :: bootstrapping :: pk"
-
-
-
-if [ "${VALIDATE_ROSETTA_HEALTH}" != "false" ] && [ ! "$(curl -s http://localhost:8080/)" ]; then
-    echo "rosetta :: bound to port :: FAIL"
-    exit 1
-fi
-echo "rosetta :: bound to port :: ok"
 echo "system :: ok"
 exit 0
