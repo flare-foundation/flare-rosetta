af39f97d8dae48c78176db15c25af18377f9278b
klemenfn <klemen@flare.network>
2022-11-25 14:50:39 +0100

    update sample run commands

--- server/run_costwo_online.sh
+++ server/run_costwo_online.sh
@@ -1,3 +1,3 @@
 docker stop costwo_online
 docker rm costwo_online
-docker run -d --name costwo_online -p 8080:8080 -p 9650:9650 -p 9651:9651 -e MODE=online -v /opt/flare/db_online:/app/flare/db flarefoundation/flare-rosetta:v0.7.1 costwo
+docker run -d --name costwo_online -p 8080:8080 -p 9650:9650 -p 9651:9651 -e NETWORK_ID=costwo -e MODE=online -v /opt/flare/db_online:/data my-local-flare-rosetta-image
